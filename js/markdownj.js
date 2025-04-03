76312ab006f4190709388730c8732b1ebbf42074
imitoy <imitoy@outlook.com>
2025-04-04 06:58:46 +0800

    4-4

--- js/markdownj.js
+++ js/markdownj.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+    var path = window.location.pathname;
+    if(!path.endsWith("/") && !path.endsWith(".html")){
+        path += "/";
+    }
+    if(path.endsWith("/")){
+        path += "index.html";
+    }
     $.get(window.location.pathname.replace("index.html", "index.md"),function(response,state){
         $("#content").html(marked.parse(response));
         MathJax.typeset();
